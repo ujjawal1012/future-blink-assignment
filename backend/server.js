54c3260684092d54286abe500ab7acc117a70991
ujjawal-55 <ujjawaljain165@gmail.com>
2026-03-26 16:52:10 +0530

    change url

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5174",
+  origin: process.env.FRONTEND_URL || "https://future-blink.netlify.app",
 }));
 app.use(express.json());
 
